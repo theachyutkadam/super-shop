e75e8f8c577dcabd135bbfb602d8c3ca74cc522e
achyut kadam <theachyutkadam@gmail.com>
2021-08-12 13:57:36 +0530

    initial login setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root :to => "pages#index"
+
+  root :to => "sessions#new"
+
+  get 'logout' => "sessions#destroy", :as => "log_out"
+  # get 'log_in' => "sessions#new", :as => "log_in"
+  # get 'sign_up' => "users#new", :as => "sign_up"
+
+  resources :users
+  resources :sessions
   resources :customers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210803154709) do
+ActiveRecord::Schema.define(version: 20210811055313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20210803154709) do
     t.date "birthdate"
     t.string "gender"
     t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
